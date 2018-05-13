6ca7beac943872fa2e9a3f591f86d578908c8d8c
phptest <876507628@qq.com>
2018-05-13 17:23:28 +0800

    获取openid

--- index.php
+++ index.php
@@ -8,21 +8,33 @@ include './config/init.php';
 
 # 微信认证
 $weixin = new WxApi();
-$weixin->valid();
+//$weixin->valid();
 
 # 如果是微信请求，跳转到微信方法里面
 $dataFromClient = isset($GLOBALS["HTTP_RAW_POST_DATA"]) ? $GLOBALS['HTTP_RAW_POST_DATA'] : file_get_contents("php://input");
 if(substr($dataFromClient, 1, 3) == 'xml' && strpos($dataFromClient, 'ToUserName')){
-    if(simplexml_load_string($dataFromClient, 'SimpleXMLElement', LIBXML_NOCDATA)){
+    if($xml = simplexml_load_string($dataFromClient)){
         $conf['index'] = array(
             'p'=>'wechat',
             'm'=>'WxApi',
             'a'=>'responseMsg'
         );
+        // 获取用户的openid
+        $weixin->getOpenid();
+        if( isset( $xml->Latitude) ){
+            $json = file_get_contents("http://api.map.baidu.com/geocoder/v2/?location={$xml->Latitude},{$xml->Longitude}&output=json&pois=1&ak=Xh2kiT2FWMD0fPRaWU2zGxYdBZCPKP87");
+            $data = json_decode($json);
+            $province = str_replace('省', '', $data['addressComponent']['province']);
+            $city = str_replace('市', '', $data['addressComponent']['city']);
+            $condition = [
+                'name' => $city
+            ];
+            $cate = M('\\mode\\CateModel')->table('table')->select($condition);
+//            M('\\model\\UserModel')->table('bl_user')->update('');
+
+        }
     }
-    if( $xml = simplexml_load_string($dataFromClient, 'SimpleXMLElement', LIBXML_NOCDATA) ){
-        file_put_contents('./location.txt', $xml->Latitude . ' - ' . $xml->Longitude);
-    }
+
 }
 
 
